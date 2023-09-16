95d617560151f7102d4a65f2024797f4429b9635
thejimi <118121627+thejimi@users.noreply.github.com>
2023-09-16 12:58:44 +0200

    #7

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ function opacityDarker(){
 	document.getElementById("content").style.opacity = 0.4
 }
 
+function showModal(id){
+	var element = document.getElementById(id)
+	element.setAttribute("x-data", "{isOpen:true}")
+	opacityDarker()
+}
+
 console.warn("script loaded")
