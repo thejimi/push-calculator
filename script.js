d77c07701f81cb100e610f8e27619887cd593b1f
thejimi <118121627+thejimi@users.noreply.github.com>
2023-09-16 12:35:21 +0200

    #2

---
+++ script.js
@@ -0,0 +1,11 @@
+function on_websiteload(){
+	opacityDarker()
+}
+
+function opacityFix(){
+	document.getElementById("content").style.opacity = 1
+}
+
+function opacityDarker(){
+	document.getElementById("content").style.opacity = 0.4
+}
