eb3bb9c17abc041449bb84c44fc15488835b9f4a
thejimi <118121627+thejimi@users.noreply.github.com>
2023-09-16 12:49:17 +0200

    #4

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function on_websiteload(){
-	setTimeout(() => {
-		document.getElementById("loading").style.display = "none"
-		document.body.classList.add("bg-black")
-		document.getElementById("everything").style.display = "block"
-	}, 500)
-}
-
 function opacityFix(){
 	document.getElementById("content").style.opacity = 1
 }
